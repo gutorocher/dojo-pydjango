aa150db7b61c3e89479239f03e3048b7da84b688
Gustavo Rocha <gustavo.rochanetto@gmail.com>
2011-11-05 14:09:48 -0200

    arquivos adicionados .py e descricao do mesmo

---
+++ dojo-day2/JogoVelha.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+JogoVelha.py
+
+Created by Gus Rocha on 2011-11-05.
+Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+import un
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
