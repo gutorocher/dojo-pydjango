f697884bd4a98a83b86c2d25599290aa6159dfd6
Gustavo Rocha <gustavo.rochanetto@gmail.com>
2011-12-04 21:47:38 -0200

    refatoracao codigo

--- dojo-day3/conversorRomano.py
+++ dojo-day3/conversorRomano.py
@@ -1,50 +1,86 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
-# aqui serão implemntada
-
+# aqui serão implementado
+# os seguintes itens.::
 # a conversão numerica
 # de romano para decimal
-# verificação de valores romanos inválidos
+# verificação de valores romanos inválidos:
+#tipos numeros e valores como A, B ...
+
 
 class Conversor(object):
 
-    ''' definir os metodos para a classe para implementar as funcionalidade do Conversão de Romano para decimal '''
+    ''' definir os metodos para a classe para implementar
+    as funcionalidade do Conversão de Romano para decimal '''
 
-    def conversorRomanoDecimal(valor):    
+    resultante = [1,5,10]
+                    
+    def conversorRomanoDecimal(self,valor):
         if valor is not None:
             tamanho = len(valor)    
-            if tamanho == 1 :
-                identificaRomano(self,valor)
-            elif tamanho == 2 :
+            if tamanho == 1:
+                if valor == 'I':
+                    return self.resultante[0]
+                elif valor == 'V':
+                    return self.resultante[1]
+                elif valor == 'X':
+                    return self.resultante [2]
+                else:
+                    print "valor invalido para valor de 1 caracter"
+            
+            if tamanho == 2 :
                 lista = []
                 lista = list(valor)
-                print lista
-                primeiro = identificaRomano(self,lista[0])
-                segundo = identificaRomano(self,lista[1])
-                if primeiro > segundo:
-                    return primeiro + segundo
-                else:
-                    return segundo - primeiro
+                print " =============================="
+                print " LISTA"
+                print " =============================="
+                print " resposta é: " + str(lista)
+                print " =============================="
                 
-            
-    def identificaRomano(self,valor):
-        if valor == 'I':
-            return resultante[0]
-        elif valor == 'V':
-            return resultante[1]
-        elif valor == 'X':
-            return resultante[2]
-        else:
-            print "valor invalido"
+                elemento1 = lista[0]
+                elemento2 = lista[1]
 
-        valor = ['I','V','X']
-        resultante = [1,5,10]
+                if elemento1 > elemento2:                
+                    
+                    if lista[0] == 'I':
+                        temp1 = self.resultante[0];return temp1
+                    elif lista[0]== 'V':
+                        temp1= self.resultante[1];return temp1
+                        print "teste" +str(temp1)
+                    elif lista[0]== 'X':
+                        temp1 =  self.resultante[2];return temp1
+                   
+                    elif lista[1]== 'I':
+                        temp2= self.resultante[0];return temp2
+                    elif lista[1] == 'V':
+                        temp2 =self.resultante[1]; return temp2
+                    elif lista[1]=='X':
+                        temp2 = self.resultante[2];return temp2
+                   
+                    print temp1 + temp2
+                    
+                else:
+                    return elemento2 - elemento1
+                    
+       
 
-
-if __name__ == '__main__':
-    conversorRomanoDecimal('V')
-
-
+if __name__ == "__main__":
     
+#    print " valor decimal de V"
+#    print Conversor().conversorRomanoDecimal('V')
+#    
+#    print "valor decimal de X"
+#    print Conversor().conversorRomanoDecimal('X')
+#    
+#    print "valor decimal de I"
+#    print Conversor().conversorRomanoDecimal('I')
+    
+    print Conversor().conversorRomanoDecimal('I')
+#    print "valor decimal de IV"
+#    print Conversor().conversorRomanoDecimal('IV')
+#    print "valor decimal de VI"
+#    print Conversor().conversorRomanoDecimal('VI')
+    
+
+
--- dojo-day3/teste.py
+++ dojo-day3/teste.py
@@ -4,10 +4,13 @@
 
 #implementar todo o teste primeiro
 
-''' teremos como foco inicial fazermos a conversão romano para decimal de 1 ao 10 e vice versa'''
-''' 1- realizar o teste para ver se a conversao foi feita corretamente: pegaremos de inicio os inteiros como I = 1 , V = 5 e X = 10 
-para depois partimos para os demias...
 
+
+''' teremos como foco inicial fazermos a conversão romano para decimal de 1 ao 10 e vice versa'''
+''' 1- realizar o teste para ver se a conversao foi feita corretamente: pegaremos de inicio os inteiros
+como I = 1 , V = 5 e X = 10 
+
+para depois partimos para os demias...
   se for I retorna 1
   se for II reorna 2
   se dor III retorna 3
@@ -18,14 +21,11 @@ para depois partimos para os demias...
   se for VIII retorna 8
   se for IX retorna 9
   se for X retorna 10
-
 '''
 
 '''2 - realizar o teste para verificar se o numero romano e valido'''
 
-
 ''' realizar teste para fazer a conversao de decimal para romano esta correta'''
-
 
 import unittest
 
@@ -35,24 +35,42 @@ class TesteClass(unittest.TestCase):
 
     ''' realizar os sempre efine os valores do objeto'''
     def setUp(self):
-         self.c = Conversor()
+        self.c = Conversor()
 
     def teste_objeto(self):
         ''' testa se existe o objeto c '''
         self.assertTrue(self.c)
 
     def testa_conversao_romano_I (self):
+        ''' testa se o resultado I = 1 '''
         self.assertEqual(self.c.conversorRomanoDecimal('I'),1)
 
+    def testa_romano_V(self):
+        '''testa conversão do V '''
+        self.assertEqual(self.c.conversorRomanoDecimal('V'),5)
+        
     def testa_conversao_romano_IV (self):
+        ''' verifica algorismo romano com dois digitos e testa se IV = 4 '''
         self.assertEqual(self.c.conversorRomanoDecimal('IV'),4)
 
-    def teste_valor_invalido(self):
-        self.assertNotEqual(self.c.conversorRomanoDecimal('A','B','C','D'),'invalido')
+    def testar_conversao_romano_VI (self):
+        ''' verifica a conversao'''
+        self.assertEqual(self.c.conversorRomanoDecimal('VI'), 6)
+        
+    def testar_entrada(self):
+        ''' testa entrada valida '''
+        self.assertTrue(self.c.conversorRomanoDecimal('A,B,C,D,E,F,G'), msg='entrada invalida')
+        
 
-    
-    def teste_valor_numerico(self):
-        self.assertEqual(self.c.conversorRomanoDecimal('1','2','3','4','5','6'),'invalido')
+#    def teste_valor_invalido(self):
+#        ''' verifica se a entrada e valida não permite entrar valores invalidos
+#        que não contem no Algorismo romano '''
+#        self.assertNotEqual(self.c.conversorRomanoDecimal('A','B','C','D'),'invalido')
+#
+#    def teste_valor_numerico(self):
+#        ''' verifica se a entrada e valida não permite entrar valores numericos '''
+#        self.assertEqual(self.c.conversorRomanoDecimal('1','2','3','4','5','6'),'invalido')
+
 
 
 
@@ -65,3 +83,4 @@ if __name__ == '__main__':
     
 
       
+
