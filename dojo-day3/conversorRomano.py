f2bd037568e9dfe0b3a5554d6b81a0524661436b
gusnetto <gustavo.rochanetto@gmail.com>
2012-01-19 00:33:23 -0200

    correct one part because i don't see the correct result, now is work.

--- dojo-day3/conversorRomano.py
+++ dojo-day3/conversorRomano.py
@@ -12,14 +12,15 @@ class Conversor(object):
     ''' definir os metodos para a classe para implementar
     as funcionalidade do Conversão de Romano para decimal '''
 
-    resultante = [1,5,10]
-                    
+   resultante = [1,5,10]
+                        
     def conversorRomanoDecimal(self,valor):
         if valor is not None:
+            # a funcao len verifica o tamanho da string 
             tamanho = len(valor)    
             if tamanho == 1:
                 if valor == 'I':
-                    return self.resultante[0]
+                   return self.resultante[0]
                 elif valor == 'V':
                     return self.resultante[1]
                 elif valor == 'X':
@@ -33,51 +34,62 @@ class Conversor(object):
                 print " =============================="
                 print " LISTA"
                 print " =============================="
-                print " resposta é: " + str(lista)
+                print " a lista empresa é: " + str(lista)
                 print " =============================="
                 
                 elemento1 = lista[0]
                 elemento2 = lista[1]
+                #resultado1 = 0
+                #resultado2 = 0
 
-                if elemento1 > elemento2:                
+
+                if elemento1 > elemento2:      
+                    
                     
                     if lista[0] == 'I':
-                        temp1 = self.resultante[0];return temp1
+                        resultado1 = 1
+                        #return resultado1
                     elif lista[0]== 'V':
-                        temp1= self.resultante[1];return temp1
-                        print "teste" +str(temp1)
+                        resultado1 = 5
+                        #return resultado1
                     elif lista[0]== 'X':
-                        temp1 =  self.resultante[2];return temp1
+                        resultado1 = 10
+                        #return resultado1
                    
                     elif lista[1]== 'I':
-                        temp2= self.resultante[0];return temp2
+                        resultado2=1
+                        #return resultado2
                     elif lista[1] == 'V':
-                        temp2 =self.resultante[1]; return temp2
+                        resultado2 =5
+                        #return resultado2
                     elif lista[1]=='X':
-                        temp2 = self.resultante[2];return temp2
+                        resultado2 = 10
+                        #return resultado2
                    
-                    print temp1 + temp2
+                    return  (resultado1) + (resultado2)
                     
                 else:
-                    return elemento2 - elemento1
+                    if elemento1 < elemento2:
+                        return resultado2 - resultado1
                     
        
 
 if __name__ == "__main__":
     
-#    print " valor decimal de V"
-#    print Conversor().conversorRomanoDecimal('V')
-#    
-#    print "valor decimal de X"
-#    print Conversor().conversorRomanoDecimal('X')
-#    
-#    print "valor decimal de I"
-#    print Conversor().conversorRomanoDecimal('I')
+    print "valor decimal de V"
+    print Conversor().conversorRomanoDecimal('V')
     
+    print "valor decimal de X"
+    print Conversor().conversorRomanoDecimal('X')
+    
+    print "valor decimal de I"
     print Conversor().conversorRomanoDecimal('I')
-#    print "valor decimal de IV"
-#    print Conversor().conversorRomanoDecimal('IV')
+
+    print "valor decimal de IV"
+    print Conversor().conversorRomanoDecimal('IV')
 #    print "valor decimal de VI"
 #    print Conversor().conversorRomanoDecimal('VI')
+    
+
     
 
